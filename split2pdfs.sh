5dd446babe7b0ab04a5d9901605fa7dfe4ef861e
Daiki Shiozawa <nacl30d@gmail.com>
2022-07-13 23:47:46 +0900

    :sparkles: ファイル名をクオーテーションしてスペース等に対応

--- split2pdfs.sh
+++ split2pdfs.sh
@@ -2,20 +2,20 @@
 
 function croppdf () {
     file=$1
-    file_name="$(basename $file .pdf)"
+    file_name="$(basename "$file" .pdf)"
 
     # set page size
-    read org_width org_height <<< $(pdfinfo $file | grep 'Page size:' | awk '{print int($3+0.5), int($5+0.5)}')
+    read org_width org_height <<< $(pdfinfo "$file" | grep 'Page size:' | awk '{print int($3+0.5), int($5+0.5)}')
     target_width=$(($org_width / 2))
     target_height=$org_height
 
     # crop left page
     echo '=> Crop left harf page'
-    pdftocairo $file -pdf -nocrop -x 0 -y 0 -W $target_width -H $target_height -paperw $target_height -paperh $target_width ./cropped/${file_name}.left.pdf
+    pdftocairo "$file" -pdf -nocrop -x 0 -y 0 -W $target_width -H $target_height -paperw $target_height -paperh $target_width "./cropped/${file_name}.left.pdf"
 
     # crop right page
     echo '=> Crop left harf page'
-    pdftocairo $file -pdf -nocrop -x $target_width -y 0 -W $target_width -H $target_height -paperw $target_height -paperh $target_width ./cropped/${file_name}.right.pdf
+    pdftocairo "$file" -pdf -nocrop -x $target_width -y 0 -W $target_width -H $target_height -paperw $target_height -paperh $target_width "./cropped/${file_name}.right.pdf"
 }
 
 
@@ -23,35 +23,35 @@ function croppdf () {
 # main script
 ###
 
-input_file=$(find `pwd` -name $1)
-input_file_name="$(basename $input_file)"
+input_file=$(find "`pwd`" -name "$1")
+input_file_name="$(basename "$input_file")"
 work_dir=${input_file_name%.*}
 output_file=${input_file_name}.split.pdf
 
 echo '=> Create work directory'
-mkdir ${work_dir}
-cd ${work_dir}
-mkdir split cropped united
+mkdir -p "${work_dir}"
+cd "${work_dir}"
+mkdir -p split cropped united
 
 # separate each page
 echo '=> Split each pages'
-pdfseparate $input_file split/%d.pdf
+pdfseparate "$input_file" split/%d.pdf
 
 # crop each page
 for pdf in split/*.pdf
 do
     if [ -f "$pdf" ]; then
-        croppdf $pdf
-        name=$(basename $pdf)
+        croppdf "$pdf"
+        name=$(basename "$pdf")
         number=${name%.*}
         echo '=> Combine left and right page'
-        pdfunite $(ls cropped/${number}.* | sort -n) united/${number}.pdf
+        pdfunite $(ls "cropped/${number}.*" | sort -n) "united/${number}.pdf"
     else
         continue
     fi
 done
 
 # rm working files
-rm -rf ${input_file_name}/split ${input_file_name}/cropped
+rm -rf "${input_file_name}/split" "${input_file_name}/cropped"
 
 exit 0
--- splitpdf.sh
+++ splitpdf.sh
@@ -2,18 +2,18 @@
 
 function croppdf () {
     file=$1
-    file_name="$(basename $file .pdf)"
+    file_name="$(basename "$file" .pdf)"
 
     # set page size
-    read org_width org_height <<< $(pdfinfo $file | grep 'Page size:' | awk '{print int($3+0.5), int($5+0.5)}')
+    read org_width org_height <<< $(pdfinfo "$file" | grep 'Page size:' | awk '{print int($3+0.5), int($5+0.5)}')
     target_width=$(($org_width / 2))
     target_height=$org_height
 
     # crop left page
-    pdftocairo $file -pdf -nocrop -x 0 -y 0 -W $target_width -H $target_height -paperw $target_height -paperh $target_width ./cropped/${file_name}.left.pdf
+    pdftocairo "$file" -pdf -x 0 -y 0 -W $target_width -H $target_height -paperw $target_height -paperh $target_width "./cropped/${file_name}.left.pdf"
 
     # crop right page
-    pdftocairo $file -pdf -nocrop -x $target_width -y 0 -W $target_width -H $target_height -paperw $target_height -paperh $target_width ./cropped/${file_name}.right.pdf
+    pdftocairo "$file" -pdf -x $target_width -y 0 -W $target_width -H $target_height -paperw $target_height -paperh $target_width "./cropped/${file_name}.right.pdf"
 }
 
 
@@ -21,18 +21,18 @@ function croppdf () {
 # main script
 ###
 
-input_file=$(find `pwd` -name $1)
-input_file_name=$(basename $input_file)
+input_file=$(find "`pwd`" -name "$1")
+input_file_name=$(basename "$input_file")
 work_dir=${input_file_name%.*}
-output_file=${input_file_name}.split.pdf
+output_file="${input_file_name}.split.pdf"
 
 
-mkdir ${work_dir}
-cd ${work_dir}
+mkdir "${work_dir}"
+cd "${work_dir}"
 mkdir split cropped
 
 # separate each page
-pdfseparate $input_file ./split/%d.pdf
+pdfseparate "$input_file" ./split/%d.pdf
 
 # crop each page
 for pdf in ./split/*.pdf
@@ -45,7 +45,7 @@ do
 done
 
 # unite hole page
-pdfunite $(ls cropped/ | sort -n | xargs printf 'cropped/%s\n') $output_file
+pdfunite $(ls cropped/ | sort -n | xargs printf 'cropped/%s\n') "$output_file"
 
 # rm working files
 rm -rf split cropped
